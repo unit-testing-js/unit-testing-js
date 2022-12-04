a02011584fb8f4c8ea8e878b5d048ee3ababa523
grh-gitee <3494242908@qq.com>
2022-12-04 19:18:17 +0800

    docs: 添加开源证书, 修改项目基础配置

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# 描述
+# unit-testing-js
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg) [![npm version](https://img.shields.io/npm/v/unit-testing-js.svg?style=flat)](https://www.npmjs.com/package/unit-testing-js)
 
 - 添加测试用例来执行是否符合预期
 - 快速生成想要的测试用例
--- commitlint.config.js
+++ commitlint.config.js
@@ -12,6 +12,7 @@ module.exports = {
         'style', // 格式（不影响代码运行的变动）
         'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
         'test', // 增加测试
+        'branch', // 创建新的分支
         'chore', // 构建过程或辅助工具的变动
         'revert', // feat(pencil): add ‘graphiteWidth’ option (撤销之前的commit)
         'merge' // 合并分支， 例如： merge（前端页面）： feature-xxxx修改线程地址
