37be3fe76c3f4decc3eb7f59212e0143d93c364b
grh-gitee <3494242908@qq.com>
2023-08-11 10:01:07 +0800

    style: 优化log日志的格式化

--- src/utils/logNotSuccess.ts
+++ src/utils/logNotSuccess.ts
@@ -2,8 +2,22 @@ import { color } from 'rh-color'
 import type { Styles } from 'rh-color'
 import type { CaseUnit } from '..'
 import { type } from 'abandonjs'
+import { isArray, isObject } from 'asura-eye'
 import { parse } from './parse'
 import { icon } from './icon'
+
+const isLogObject = (value: unknown) => isObject(value) || isArray(value)
+
+const logObject = (value: Record<string, any> | any[]) => {
+	const list = JSON.stringify(value, null, 2).split('\n')
+	list.forEach((item, index) => {
+		if (index + 1 === list.length) {
+			console.log('   ' + item + color(`   <${type(value)}>`, 'Grey'))
+			return
+		}
+		console.log('   ' + item)
+	})
+}
 
 export function logNotSuccess(list: CaseUnit[] = [], tagColor: Styles, tagFlag) {
 	if (list.length === 0) return;
@@ -14,9 +28,6 @@ export function logNotSuccess(list: CaseUnit[] = [], tagColor: Styles, tagFlag) 
 		console.log(
 			' ' + color(icon[tagFlag], tagColor) +
 			color(` ${name} `, tagColor) +
-			// color('actual:' + ' ' + parse(actual, false) + ' ', 'Magenta') +
-			// color('tobe:' + ' ' + parse(tobe, false) + ' ', 'Cyan') +
-			// color(`${error}`, 'Red') +
 			color(` ${runTime}`, 'Grey')
 		)
 		if (param) {
@@ -25,7 +36,19 @@ export function logNotSuccess(list: CaseUnit[] = [], tagColor: Styles, tagFlag) 
 			console.log(color('  params: ', 'Magenta') + paramsString)
 		}
 
-		console.log(color('  tobe: ', 'Yellow') + parse(tobe) + color(` <${type(tobe)}>`, 'Grey'))
-		console.log(color('  actual: ', 'Cyan') + parse(actual) + color(` <${type(actual)}>`, 'Grey'))
+		if (isLogObject(tobe)) {
+			console.log(color('  tobe: ', 'Yellow'))
+			logObject(tobe)
+		} else {
+			console.log(color('  tobe: ', 'Yellow') + parse(tobe) + color(` <${type(tobe)}>`, 'Grey'))
+		}
+
+		if (isLogObject(actual)) {
+			console.log(color('  actual: ', 'Cyan'))
+			logObject(actual)
+		} else {
+			console.log(color('  actual: ', 'Cyan') + parse(actual) + color(` <${type(actual)}>`, 'Grey'))
+
+		}
 	})
 }
